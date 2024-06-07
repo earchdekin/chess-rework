061501503ea71a6e18596a02bb320f9e81a063ab
earchdekin <emily.archdekin@gmail.com>
2024-06-06 17:54:33 -0700

    add Button.py. Create functionality checking for mouse position inside of button

---
+++ Button.py
@@ -0,0 +1,16 @@
+class Button():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.w = width
+        self.h = height
+
+    #Check if the mouse position is within the correct boundary
+    #returns true if the mouse position is inside of the button
+    def mouse_is_in_boundary(self, position):
+        x = position[0]
+        y = position[1]
+        if x >= self.x and y >= self.y:
+            if x < self.width and y < self.height:
+                return True
+        return False
